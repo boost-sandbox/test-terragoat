16e75643c50de9cddb7aaa560f4a818ccbe054c3
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2022-06-03 15:44:16 -0400

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -117,13 +117,10 @@ resource aws_subnet "eks_subnet2" {
 resource aws_eks_cluster "eks_cluster" {
   name     = local.eks_name.value
   role_arn = "${aws_iam_role.iam_for_eks.arn}"
-
-  
   vpc_config {
     endpoint_private_access = true
     subnet_ids              = ["${aws_subnet.eks_subnet1.id}", "${aws_subnet.eks_subnet2.id}"]
   }
-
   depends_on = [
     "aws_iam_role_policy_attachment.policy_attachment-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.policy_attachment-AmazonEKSServicePolicy",
